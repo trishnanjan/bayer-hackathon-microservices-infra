465fe6992afd558fe1d23561d3df621234012b99
trishnanjan <trishnanjan.96@gmail.com>
2025-12-11 16:44:31 +0530

    Fix

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,18 +1,9 @@
-/*
-  Backend configuration for the main Terraform code.
-
-  IMPORTANT: You cannot configure this backend until the S3 bucket and DynamoDB table
-  exist. If you already created them, initialize Terraform with the backend-config options.
-
-*/
-
-/* Example backend block if you prefer to commit it with real values (NOT recommended):
 terraform {
   backend "s3" {
-    bucket         = "REPLACE_WITH_BUCKET"
-    key            = "patient-service/terraform.tfstate"
+    bucket         = "bayer-hackathon-tfstate"
+    key            = "${var.service}/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "REPLACE_WITH_DDB_TABLE"
+    dynamodb_table = "bayer-hackathon-state-ddb"
     encrypt        = true
   }
 }
